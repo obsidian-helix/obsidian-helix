2328f96db5b5d9730dfcd0f0e89061607fae6ea1
Marc Esquerrà <esquerra@gmail.com>
2025-11-25 18:47:40 +0000

    Start testing the actual code (#33)

---
+++ src/logic.ts
@@ -0,0 +1,21 @@
+import { helix } from 'codemirror-helix';
+import { Extension, Prec } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
+
+export interface HelixSettings {
+    enableHelixKeybindings: boolean;
+    cursorInInsertMode: "block" | "bar";
+}
+
+
+export const DEFAULT_SETTINGS: HelixSettings = {
+    enableHelixKeybindings: false,
+    // Following the defualt Obsidian behavior, instead of the Helix one.
+    cursorInInsertMode: "bar",
+}
+
+export const DEFAULT_EDITOR_VIEW = EditorView.theme({
+    ".cm-hx-block-cursor .cm-hx-cursor": {
+        background: "var(--text-accent)",
+    },
+});
