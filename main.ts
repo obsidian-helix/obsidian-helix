b45ba96ca6227b9655f6fdf16e9f9458cdb4c7a0
Marc Esquerrà <esquerra@gmail.com>
2026-01-18 22:18:17 +0000

    Fix linting issues for plugin publishing (#41)
    * Remove disable linting clauses
    
    * Fix acual linting issues

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ export default class HelixPlugin extends Plugin {
     }
 
     async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()); // eslint-disable-line
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()) as HelixSettings;
     }
 
     async saveSettings() {
