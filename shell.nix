c21bc169777003c50f4b139caed6108dcf916faa
Marc Esquerrà <esquerra@gmail.com>
2026-05-29 17:51:07 +0100

    Fix the linting configuration (#63)

--- shell.nix
+++ shell.nix
@@ -8,9 +8,6 @@ pkgs.mkShellNoCC {
   name = "obsidian-helix-dev-shell";
   nativeBuildInputs = [
     pkgs.nodejs_24
-    pkgs.yarn
-    pkgs.eslint
-    pkgs.esbuild
     jq
   ];
   shellHook = ''
